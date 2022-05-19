449b94acd1076834bfd75ae4a4402cc3a5fb5b07
hsnkhvc <44241531+hsnkhvc@users.noreply.github.com>
2022-05-19 18:37:57 +0300

    sql-patikaEgitim

---
+++ index.sql
@@ -0,0 +1,26 @@
+--1.soru
+Select Distinct replacement_cost
+From film
+
+--2.Soru
+Select Count (Distinct replacement_cost)
+From film
+
+--3.soru
+Select count(*)
+From film
+Where title like 'T%' and rating = 'G'
+
+--4.Soru
+Select count(*)
+From country
+Where country like '_____'
+
+--5.Soru
+Select count(*)
+From city
+Where city ILIKE '%r'
+
+
+
+
